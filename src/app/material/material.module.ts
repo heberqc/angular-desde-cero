4081ca53e71212dad9a9c3c3eeb3508d1040b4f3
Heber Quequejana <heberqc@gmail.com>
2019-02-07 06:03:38 -0500

    generar componentes input field

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,15 +6,18 @@ import {
   MatButtonModule,
   MatIconModule,
   MatExpansionModule,
+  MatInputModule
 } from '@angular/material';
 
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
 import { MaterialButtonComponent } from './material-button/material-button.component';
+import { MaterialInputComponent } from './material-input/material-input.component';
 
 @NgModule({
   declarations: [
     MaterialDemoComponent,
     MaterialButtonComponent,
+    MaterialInputComponent,
   ],
   imports: [
     CommonModule,
@@ -22,12 +25,14 @@ import { MaterialButtonComponent } from './material-button/material-button.compo
     MatButtonModule,
     MatIconModule,
     MatExpansionModule,
+    MatInputModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatExpansionModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }

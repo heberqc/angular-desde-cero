b136179e008ba1c87789472ccdd4eb1f409d392e
Heber Quequejana <heberqc@gmail.com>
2019-02-06 20:24:51 -0500

    generar componentes button

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,22 +5,29 @@ import {
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
+  MatExpansionModule,
 } from '@angular/material';
 
 import { MaterialDemoComponent } from './material-demo/material-demo.component';
+import { MaterialButtonComponent } from './material-button/material-button.component';
 
 @NgModule({
-  declarations: [MaterialDemoComponent],
+  declarations: [
+    MaterialDemoComponent,
+    MaterialButtonComponent,
+  ],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatExpansionModule,
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-]
+    MatExpansionModule,
+  ]
 })
 export class MaterialModule { }
